64c284ef01b27447aeab0a2ce055ed5ff050c2c7
wolfiee42 <saifalislam2022@gmail.com>
2024-06-17 15:56:49 +0600

    implementing jwt in live scenario

---
+++ src/modules/auth/auth.interface.ts
@@ -0,0 +1,4 @@
+export type TUserLogin = {
+    email: string,
+    password: string
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { USER_Role } from "../user/user.constants";
 import { TUser } from "../user/user.interface";
 import { User } from "../user/user.model";
-
+import { TUserLogin } from "./auth.interface";
+import jwt from 'jsonwebtoken';
+import { isPasswordCorrect } from "./auth.utils";
+import config from "../../config";
 const register = async (payload: TUser) => {
 
     // checking whether user already created with the given email or not.
@@ -16,6 +19,39 @@ const register = async (payload: TUser) => {
     // creating the actual user
     const newUser = await User.create(payload);
     return newUser;
+
+};
+
+const login = async (payload: TUserLogin) => {
+
+    // checking whether the user is available in out Database
+    const user = await User.findOne({ email: payload.email }).select('+password');
+    if (!user) {
+        throw new Error('User is not found')
+    }
+
+    // checking whether the user is blocked or not
+    if (user.status === 'BLOCKED') {
+        throw new Error('User is blocked')
+    }
+
+    // checking whether the password is correct
+    const passwordCheck = await isPasswordCorrect(payload.password, user.password);
+    if (!passwordCheck) {
+        throw new Error('Password is not correct')
+    }
+
+    // creating a token so that we can send frontend a information
+    const jwtPayload = {
+        email: user.email,
+        role: user.role
+    }
+
+    const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, { expiresIn: config.jwt_access_expires_in });
+    const refreshToken = jwt.sign(jwtPayload, config.jwt_refresh_secret as string, { expiresIn: config.jwt_refresh_expires_in });
+
+
+    return user;
 
 }
 
